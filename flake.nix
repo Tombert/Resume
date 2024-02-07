4b2e1c089e056487f755bd252f918c26189bc3a9
Thomas Gebert <thomas@gebert.app>
2024-02-07 13:21:18 -0500

    Mostly get flake working.

--- flake.nix
+++ flake.nix
@@ -1,24 +1,41 @@
 {
-  description = "Resume editing and building environment";
+    description = "Resume editing and building environment";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-    # Define an environment for editing your resume with LaTeX
-    devShell = {
-      packages = [
-        nixpkgs.texlive.combined.scheme-full
-      ];
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    # Define a derivation to build your resume
-    resume = {
-      outputs = [ "out" ];
-      inputDrvs = [ nixpkgs.texlive.combined.scheme-full ];
-      builder = ./build.sh;
-    };
-  };
+    outputs = { self, nixpkgs, flake-utils, ...}: 
+        flake-utils.lib.eachDefaultSystem(system:
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                };
+            in rec
+                { 
+                    devShell = pkgs.mkShell {
+                        buildInputs = [
+                            pkgs.texlive.combined.scheme-full
+                        ];
+                    };
+
+                    defaultPackage = pkgs.stdenv.mkDerivation {
+                                name = "Resume Build";
+                                src = self; 
+                                phases = ["buildPhase" "installPhase"];
+                                buildInputs = [
+                                    pkgs.texlive.combined.scheme-full
+                                ];
+
+                                buildPhase = ''
+                                    pdflatex ./resume.tex
+                                '';
+
+                                installPhase = ''
+                                    mkdir -p $out
+                                    cp resume.pdf $out
+                                '';
+                     };
+         });
 }
-
